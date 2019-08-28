dda66c81d986d6486d4a0ec2d6e8ad6e975521a5
Hendi Santika <hendisantika@gmail.com>
2019-08-29 06:36:19 +0700

    #6 - Add Return the image from the database using ResponseEntity method in ImageController

--- src/main/java/com/hendisantika/springbootimage/controller/ImageController.java
+++ src/main/java/com/hendisantika/springbootimage/controller/ImageController.java
@@ -1,9 +1,17 @@
 package com.hendisantika.springbootimage.controller;
 
+import com.hendisantika.springbootimage.entity.User;
 import com.hendisantika.springbootimage.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +28,20 @@ public class ImageController {
 
     @Autowired
     private ImageRepository imageRepository;
+
+    // Return the image from the database using ResponseEntity
+    @GetMapping("database/{id}")
+    public ResponseEntity<byte[]> fromDatabaseAsResEntity(@PathVariable("id") Integer id) throws SQLException {
+
+        Optional<User> primeMinisterOfIndia = imageRepository.findById(id);
+        byte[] imageBytes = null;
+        if (primeMinisterOfIndia.isPresent()) {
+
+            imageBytes = primeMinisterOfIndia.get().getPhoto().getBytes(1,
+                    (int) primeMinisterOfIndia.get().getPhoto().length());
+        }
+
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
+    }
 
 }
