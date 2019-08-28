2496d9d84e01048246ed2a4bba89c6e45a35d663
Hendi Santika <hendisantika@gmail.com>
2019-08-29 06:31:05 +0700

    #1 - Initial Project Setup

---
+++ src/test/java/com/hendisantika/springbootimage/SpringbootImageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootimage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootImageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
